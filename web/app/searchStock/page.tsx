f588be234b9fe5e6a9cf6fd88a70501ee4bc4b8c
van-lian <vanness.christian@outlook.com>
2025-12-16 03:14:19 +0700

    search-stock

---
+++ web/app/searchStock/page.tsx
@@ -0,0 +1,14 @@
+import SearchBar from "@/components/news-list/searchbar";
+import { HoldAsset } from "@/components/porto/holding";
+import Menubar from "@/components/navbar/menubar";
+
+export default function Page() {
+    return (
+        <div className="px-3.5 space-y-5">
+        <SearchBar />
+        <HoldAsset />
+        <div className="h-16"></div>
+        <Menubar />
+        </div>
+    );
+}
